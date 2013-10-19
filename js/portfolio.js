8a46a8ca603dfe83a5bf9f8eddb72e390ce91b84
Adam Dziendziel <adam.dziendziel@gmail.com>
2013-10-19 17:50:56 +0200

    Wyswietlanie tytulu po najechaniu myszka

--- js/portfolio.js
+++ js/portfolio.js
@@ -1,30 +1,31 @@
 angular.module('portfolio', ['ngRoute']).
     config(function($routeProvider, $interpolateProvider){
         $interpolateProvider.startSymbol('{[').endSymbol(']}');
-        $routeProvider.
+       /* $routeProvider.
             when('/:category', {
                 templateUrl: 'posts',
                 controller: 'Posts',
                 resolve: {
                     posts: function($http) {
-                          return $http({method: 'GET', url: '/posts.json'}).
-                            success(function(data, status, headers, config) {
-                              // console.log(data);
-                               // $scope.posts = data;
-                            }).
-                            error(function(data, status, headers, config) {
-                                alert('We are sorry. Could not load portfolio. Please try again by refreshing the page.')
-                            });
+
                     }
                 }
             }).
             otherwise({
                 redirectTo: '/illustrations'
-            });
+            }); */
     }).
-    controller('Posts', function(posts, $scope, $routeParams) {
-        $scope.posts = posts.data;
+    controller('Posts', function($scope, $http, $routeParams) {
+        //$scope.posts = posts.data;
 
+            $http({method: 'GET', url: '/posts.json'}).
+                            success(function(data, status, headers, config) {
+                              // console.log(data);
+                                $scope.posts = data;
+                            }).
+                            error(function(data, status, headers, config) {
+                                alert('We are sorry. Could not load portfolio. Please try again by refreshing the page.')
+                            });
 
         //$anchorScroll();
     }).
