0b64496d06a50e00a1cb88d997867c50f9973f66
Adam Dziendziel <adam.dziendziel@gmail.com>
2013-10-23 11:00:03 +0200

    smooth scrolling

--- js/portfolio.js
+++ js/portfolio.js
@@ -114,6 +114,20 @@ $(document)
         e.stopPropagation();
     });
 
+// smooth scrolling to anchor links
+$('a[href*=#]:not([href=#])').click(function() {
+if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+  var target = $(this.hash);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+  if (target.length) {
+    $('html,body').animate({
+      scrollTop: target.offset().top
+    }, 1000);
+    return false;
+  }
+}
+});
+
 (function($, window, document, undefined) {
     var $window = $(window);
 
